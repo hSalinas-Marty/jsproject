769f46fcb99b84219125c76f23dff728af45f632
pvictori <victoria.pasquaud@ynov.com>
2025-05-13 10:20:25 +0200

    data

---
+++ airbnb/routes/logements.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var logements = require('../data/logements.')
+
+/* GET home page. */
+router.get('/logements/:id', function(req, res) {
+  const id = parseInt(req.params.id) ;
+  const logement = logements.find(l=>l.id === id) ;
+  if (!logement){
+    return res.status(404).send('logement non trouver');
+  }
+  res.render('detail', {logement})
+});
+
+module.exports = router;
