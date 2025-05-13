14092a57fae399c08c2d0c05a4e8482b446d4a2b
salhugo <hugo.salinasmarty@ynov.com>
2025-05-13 10:10:52 +0200

    premier apperçu page accueil

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
+// Configuration des fichiers statiques
+app.use(express.static(path.join(__dirname, 'airbnb', 'public')));
+
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur airbnc')
-})
+  res.sendFile(path.join(__dirname, 'airbnb', 'views', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
