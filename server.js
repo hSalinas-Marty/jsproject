53739f7565bd73671aba7ea126c1082270fae4d2
pvictori <victoria.pasquaud@ynov.com>
2025-05-13 13:26:22 +0200

    ca marche

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-// Configuration des fichiers statiques
+// Importer les données
+const logements = require('./airbnb/data/logements');
+
+// Définir le moteur de template EJS
+app.set('views', path.join(__dirname, 'airbnb', 'views'));
+app.set('view engine', 'ejs');
+
+// Servir les fichiers statiques (CSS, images...)
 app.use(express.static(path.join(__dirname, 'airbnb', 'public')));
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'airbnb', 'views', 'index.html'));
+  res.render('index', { logements });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
